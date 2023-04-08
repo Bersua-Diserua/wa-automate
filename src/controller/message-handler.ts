fa3df15f5c5657ee4278848c9676cda8262b2d25
reiyanyan <hariyantoriyan.hr@gmail.com>
2023-04-08 14:51:24 +0700

    feat: bot

--- src/controller/message-handler.ts
+++ src/controller/message-handler.ts
@@ -3,17 +3,17 @@ import {
   handlerGetDefaultMessage,
   handlerGetResponseByCommand,
 } from "../lib/handler-backend.lib"
-import { obtainCustomerByPhoneNumber } from "../package/customer"
-import { jidToPhone } from "../utils/parse-number-jid"
 
 import Logger from "../utils/logger"
 import { isKeyAlive } from "../packages/redis/utils"
+import { jidToPhone } from "../utils/parse-number-jid"
+import { obtainCustomerByPhoneNumber } from "../package/customer"
 import { redisClient } from "../packages/redis"
 import { timeToExpire } from "./internal"
 
 type SeruaMessage = { commandCode?: number; customer: Customer } & WAMessage
 
-const messageHandler = async (socket: WASocket, message: WAMessage) => {
+const messageHandler = async (message: WAMessage) => {
   const jid = message.key.remoteJid!
   const phoneNumber = jidToPhone(jid)
 
@@ -24,12 +24,10 @@ const messageHandler = async (socket: WASocket, message: WAMessage) => {
     parsedMessage.customer = customer
 
     if (await isKeyAlive(jidToPhone(jid))) {
-      console.log("alive")
       redisClient.set(jidToPhone(jid), "live-assist", "EX", timeToExpire)
       return
     } else {
-      console.log("die")
-      if (parsedMessage) {
+      if (parsedMessage && !isNaN(parsedMessage.commandCode)) {
         await handlerGetResponseByCommand(
           jidToPhone(jid),
           parsedMessage.commandCode
--- src/packages/redis/utils.ts
+++ src/packages/redis/utils.ts
@@ -1,9 +1,8 @@
-import { redisClient } from "./connection";
+import { redisClient } from "./connection"
 
 const isKeyAlive = async (key: string) => {
-  const key1 = await redisClient.ttl(key);
-  console.log({ key1, key });
-  return key1 > 0;
-};
+  const key1 = await redisClient.ttl(key)
+  return key1 > 0
+}
 
-export { isKeyAlive };
+export { isKeyAlive }
