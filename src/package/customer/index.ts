14fdde92bac8dc802076bdee43bc7548971d0e3b
Rei Yan <hariyantoriyan.hr@gmail.com>
2023-05-13 23:22:34 +0700

    chore: debug

--- src/package/customer/index.ts
+++ src/package/customer/index.ts
@@ -12,10 +12,15 @@ const customerSchema = z.object({
 export type CustomerSchema = z.infer<typeof customerSchema>
 
 export async function obtainCustomerByPhoneNumber(phoneNumber: string) {
-  const { data } = await api.get("/customer/data", {
+  const { data, status } = await api.get("/customer/data", {
     params: { phoneNumber },
   })
-
+  
+  console.log({
+    data,
+    status
+  })
+  
   const validate = customerSchema.safeParse(data.payload.customer)
   if (!validate.success) {
     console.log(validate.error)
